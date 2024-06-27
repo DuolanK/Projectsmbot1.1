6981a5a51f8f0d1376fd6e6d56fe8df66ff072ca
DuolanK <duolan.kharitonov@mail.ru>
2024-06-27 16:31:30 +0900

    chatgpt

--- handlers/users/buttons.py
+++ handlers/users/buttons.py
@@ -1,28 +1,26 @@
 from aiogram import types
 from loader import dp
 import requests
-import openai
+from openai import OpenAI
+
+
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from data.config import OPENAI_API_KEY
 
 from aiogram.dispatcher.filters import Text
 
-
-
-openai.api_key = OPENAI_API_KEY
+client = OpenAI(api_key=OPENAI_API_KEY)
 storage = MemoryStorage()
-
-
-
 
 # Функция для генерации ответа с помощью OpenAI API
 def generate_response_from_openai(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=150
-    )
-    generated_text = response.choices[0].text.strip()
+    response = client.chat.completions.create(model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt}
+    ],
+    max_tokens=150)
+    generated_text = response.choices[0].message.content.strip()
     return generated_text
 
 # Обработчик для команды "Получить прогноз"
@@ -33,26 +31,19 @@ async def get_forecast(message: types.Message):
     await message.answer(f'Привет {message.from_user.full_name}! \n'
                          f'Лови свой прогноз!\n{response}')
 
-
-
-
 @dp.message_handler(text='subscribe')
 async def buttons(message: types.Message):
     await message.answer(f'Привет {message.from_user.full_name}! \n'
-                         f' Youre subscibed')
+                         f'You are subscribed')
 
 @dp.message_handler(text='unsubscribe')
 async def buttons(message: types.Message):
     await message.answer(f'Привет {message.from_user.full_name}! \n'
-                         f' Youre unsubscibed')
-
-
-
+                         f'You are unsubscribed')
 
 def generate_response_locally(input_text):
-    print('чтото живо')
-    url = 'http://localhost:8501'  # Замените на адрес вашего локального сервера
+    url = 'http://localhost:8501'
     data = {'input_text': input_text}
     response = requests.post(url, json=data)
-    generated_text = response.json().get('generated_text')
+    generated_text = response.json().generated_text
     return generated_text
